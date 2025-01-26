702281b5f1a47c9ced3dbbd994357458c05008e8
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2025-01-25 20:30:00 -0500

    Update IVCalculator.py

--- IVCalculator.py
+++ IVCalculator.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from PIL import ImageTk
+from PIL import Image, ImageTk
 
 # Window Setup
 root = Tk()
@@ -16,32 +16,61 @@ backgroundLabel.place(relx = 0.5, rely = 0.5, anchor = "center")
 evValues = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
 for value in evValues:
     value.set(0)
+    
+# Container for the EV labels.
+evLabels = Label(backgroundLabel, padx = 0, pady = 0)
+evLabels.place(relx = 0.775, rely = 0.578, anchor = "center")
+evValueLabels = []
+for i in range (0, 6):
+    evValueLabels.append(Label(evLabels, textvariable = evValues[i], font = ("Arial", 20), fg = "white"))
+    evValueLabels[i].place(anchor = "center")
+    if i == 0:
+        evValueLabels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
+    elif i == 5:
+        evValueLabels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
+    else:
+        evValueLabels[i].pack(side = "top", padx = 7, pady = 3.5)
+
 
 # Initialize IV Variables
 ivValues = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
 for value in ivValues:
     value.set(0)
 
-ivLabels = Label(backgroundLabel)
-ivLabels.place(relx=0.91, rely=0.568, anchor="center")
-# Create a label for the number, without any background (to make it "invisible")
-label1 = Label(ivLabels, textvariable=ivValues[0], font=("Arial", 20), fg="white", text="Transparent Text", bg=root['bg'])
-label1.place(anchor="center")
-label1.pack(side = "top", pady=4)
-label2 = Label(ivLabels, textvariable=ivValues[1], font=("Arial", 20), fg="white", text="Transparent Text", bg=root['bg'])
-label2.place(relx=0.5, rely=0.5, anchor="center")
-label2.pack(side = "top", pady=4)
-label3 = Label(ivLabels, textvariable=ivValues[2], font=("Arial", 20), fg="white", text="Transparent Text", bg=root['bg'])
-label3.place(relx=0.5, rely=0.5, anchor="center")
-label3.pack(side = "top", pady=4)
-label4 = Label(ivLabels, textvariable=ivValues[3], font=("Arial", 20), fg="white", text="Transparent Text", bg=root['bg'])
-label4.place(relx=0.5, rely=0.5, anchor="center")
-label4.pack(side = "top", pady=4)
-label5 = Label(ivLabels, textvariable=ivValues[4], font=("Arial", 20), fg="white", text="Transparent Text", bg=root['bg'])
-label5.place(relx=0.5, rely=0.5, anchor="center")
-label5.pack(side = "top", pady=4)
-label6 = Label(ivLabels, textvariable=ivValues[5], font=("Arial", 20), fg="white", text="Transparent Text", bg=root['bg'])
-label6.place(relx=0.5, rely=0.5, anchor="center")
-label6.pack(side = "top", pady=4)
+# Container for the IV labels.
+ivLabels = Label(backgroundLabel, padx = 0, pady = 0)
+ivLabels.place(relx = 0.91, rely = 0.578, anchor = "center")
+ivValueLabels = []
+for i in range(0, 6):
+    ivValueLabels.append(Label(ivLabels, textvariable = ivValues[i], font = ("Arial", 20), fg = "white"))
+    ivValueLabels[i].place(anchor = "center")
+    if i == 0:
+        ivValueLabels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
+    elif i == 5:
+        ivValueLabels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
+    else:
+        ivValueLabels[i].pack(side = "top", padx = 7, pady = 3.5)
+
+selectedPokemon = StringVar(root)
+selectedPokemon.set("Bulbasaur")
+pokemonList = []
+pokemonList.append("Bulbasaur")
+pokemonList.append("Ivysaur")
+pokemonList.append("Venusaur")
+pokemonList.append("Charmander")
+pokemonList.append("Charmeleon")
+pokemonList.append("Charizard")
+pokemonList.append("Squirtle")
+pokemonList.append("Wartortle")
+pokemonList.append("Blastoise")
+pokemonList.append("Caterpie")
+pokemonList.append("Metapod")
+pokemonList.append("Butterfree")
+pokemonList.append("Weedle")
+pokemonList.append("Kakuna")
+pokemonList.append("Beedrill")
+
+w = OptionMenu(backgroundLabel, selectedPokemon, *pokemonList)
+w.place(relx = 0.225, rely = 0.25, anchor='center')
 
 root.mainloop()
