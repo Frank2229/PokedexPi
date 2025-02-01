f6863c29e5119e3ad016519e5b104b7fd7ff94a9
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2025-01-31 21:18:58 -0500

    Update IVCalculator.py

--- IVCalculator.py
+++ IVCalculator.py
@@ -2,22 +2,50 @@ from tkinter import *
 from PIL import Image, ImageTk
 import math
 
-def calculateIVs(pokemon):
+def calculate_ivs(pokemon):
     '''
     This function calculates the IVs of a given pokemon given all stats and using the in-game formula.
     Before calculations, multipliers are established for each stat based on the pokemon's nature.
     HP calculations are done separately since the formula is different.
     '''
-    multipliers = getNatureMult(pokemon[1])
-    pokemonIVs = []
-    pokemonIVs.append(math.ceil((100 * (pokemon[4][0] - pokemon[2] - 10)) / pokemon[2]) - (2 * pokemon[3][0]) - (int)(pokemon[5][0] / 4))
-    pokemonIVs[0] = int(pokemonIVs[0])
+    multipliers = get_nature_mult(pokemon[1])
+    pokemon_ivs = []
+    pokemon_ivs.append(math.ceil((100 * (pokemon[4][0] - pokemon[2] - 10)) / pokemon[2]) - (2 * pokemon[3][0]) - (int)(pokemon[5][0] / 4))
+    pokemon_ivs[0] = int(pokemon_ivs[0])
     for i in range(1, 6):
-        pokemonIVs.append(((100.0 * (math.ceil(pokemon[4][i] / multipliers[i - 1]) - 5.0)) / pokemon[2]) - (2.0 * pokemon[3][i]) - (int)(pokemon[5][i] / 4.0))
-        pokemonIVs[i] = int(pokemonIVs[i])
-    return pokemonIVs
+        pokemon_ivs.append(((100.0 * (math.ceil(pokemon[4][i] / multipliers[i - 1]) - 5.0)) / pokemon[2]) - (2.0 * pokemon[3][i]) - (int)(pokemon[5][i] / 4.0))
+        pokemon_ivs[i] = int(pokemon_ivs[i])
 
-def getNatureMult(nature):
+    return pokemon_ivs
+
+
+def load_my_pokemon_data():
+    '''
+    Load user data.
+    If the file does not exist, create one.
+    '''
+    my_pokemon_dict = {}
+    try:
+        with open("MyPokemon.txt", 'r') as file:
+            for line in file:
+                [pokemon_names, nature, level, base, stats, evs] = line.strip().split(":")
+                base_list = base.strip().split(",")
+                stats_list = stats.strip().split(",")
+                evs_list = evs.strip().split(",")
+                level = int(level)
+                for i in range(0, 6):
+                    base_list[i] = int(base_list[i])
+                    stats_list[i] = int(stats_list[i])
+                    evs_list[i] = int(evs_list[i])
+                my_pokemon_dict[pokemon_names.rstrip()] = (nature, level, base_list, stats_list, evs_list)
+    except FileNotFoundError:
+        with open("MyPokemon.txt", 'w') as file:
+            file.write("")
+
+    return my_pokemon_dict
+
+
+def get_nature_mult(nature):
     '''
     This function determins stat multipliers based on a pokemon's nature.
     Returns a list of all stat multipliers except HP.
@@ -84,158 +112,133 @@ def getNatureMult(nature):
 
     return multipliers
 
-# Window Setup
-root = Tk()
-root.title("IV Calculator")
-root.geometry("480x320")
-#root.attributes("-fullscreen", True)
 
-# Background Image
-backgroundImage = PhotoImage(file = "MenuBackground.png")
-backgroundLabel = Label(root, image = backgroundImage)
-backgroundLabel.place(relx = 0.5, rely = 0.5, anchor = "center")
+def main():
+    root = Tk()
+    root.title("IV Calculator")
+    root.geometry("480x320")
+    #root.attributes("-fullscreen", True)
 
-'''
-Load user data.
-If the file does not exist, create one.
-'''
-myPokemonDict = {}
-try:
-    with open("MyPokemon.txt", 'r') as file:
-         for line in file:
-            [pokemonNames, nature, level, base, stats, evs] = line.strip().split(":")
-            baseList = base.strip().split(",")
-            statsList = stats.strip().split(",")
-            evsList = evs.strip().split(",")
-            level = int(level)
-            for i in range(0, 6):
-                baseList[i] = int(baseList[i])
-                statsList[i] = int(statsList[i])
-                evsList[i] = int(evsList[i])
-            myPokemonDict[pokemonNames.rstrip()] = (nature, level, baseList, statsList, evsList)
-except FileNotFoundError:
-    with open("MyPokemon.txt", 'w') as file:
-        file.write("")
+    background_image = PhotoImage(file = "MenuBackground.png")
+    background_label = Label(root, image = background_image)
+    background_label.place(relx = 0.5, rely = 0.5, anchor = "center")
 
-'''
-Take the extracted file data and organize into a list of tuples.
-Tuple indices: 0 = name, 1 = level, 2 = evs list
-'''
-myPokemonList = []
-temp = 0
-for key in myPokemonDict:
-    myPokemonList.append((key, myPokemonDict.get(key)[0], myPokemonDict.get(key)[1], list(myPokemonDict.get(key))[2], list(myPokemonDict.get(key))[3], list(myPokemonDict.get(key))[4]))
+    my_pokemon_dict = load_my_pokemon_data()
 
-'''
-Create the Pokemon selector.
-This block iterates through a .txt to retieve all pokemon for the optionmenu.
-'''
-selectedPokemon = StringVar(root)
-if len(myPokemonList) == 0:
-    selectedPokemon.set("Bulbasaur")
-else:
-    selectedPokemon.set(myPokemonList[0][0])
-pokemonList = []
-with open("NationalPokedex.txt", "r") as file:
-    for line in file:
-        pokemonList.append(line.strip())
-pokemonListSelect = OptionMenu(backgroundLabel, selectedPokemon, *pokemonList)
-pokemonListSelect.place(relx = 0.125, rely = 0.25, anchor='center')
+    '''
+    Take the extracted file data and organize into a list of tuples.
+    Tuple indices: 0 = name, 1 = level, 2 = evs list
+    '''
+    my_pokemon_list = []
+    temp = 0
+    for key in my_pokemon_dict:
+        my_pokemon_list.append((key, my_pokemon_dict.get(key)[0], my_pokemon_dict.get(key)[1], list(my_pokemon_dict.get(key))[2], list(my_pokemon_dict.get(key))[3], list(my_pokemon_dict.get(key))[4]))
 
-# Setup nature
-natureSelected = StringVar(root)
-if len(myPokemonList) == 0:
-    natureSelected.set("Bulbasaur")
-else:
-    natureSelected.set(myPokemonList[0][1])
-natureList = []
-with open("Natures.txt", "r") as file:
-    for line in file:
-        natureList.append(line.strip())
-naturesListSelect = OptionMenu(backgroundLabel, natureSelected, *natureList)
-naturesListSelect.place(relx = 0.325, rely = 0.25, anchor = "center")
-
-# Setup level
-pokemonLvl = IntVar()
-if len(myPokemonList) == 0:
-    pokemonLvl.set(0)
-else:
-    pokemonLvl.set(myPokemonList[0][2])
-lvlLabel = Label(backgroundLabel, padx = 7, pady = 0, textvariable = pokemonLvl, font = ("Arial", 14), fg = "white", bg = "#1f5387")
-lvlLabel.place(relx = 0.6, rely = 0.185, anchor = "center")
-
-# Initialize Stat Variables
-statValues = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
-temp = 0
-for value in statValues:
-    if len(myPokemonList) == 0:
-        value.set(0)
+    '''
+    Create the Pokemon selector.
+    This block iterates through a .txt to retieve all pokemon for the optionmenu.
+    '''
+    selected_pokemon = StringVar(root)
+    if len(my_pokemon_list) == 0:
+        selected_pokemon.set("Bulbasaur")
     else:
-        value.set(myPokemonList[0][4][temp])
-    temp += 1
+        selected_pokemon.set(my_pokemon_list[0][0])
+    pokemon_list = []
+    with open("NationalPokedex.txt", "r") as file:
+        for line in file:
+            pokemon_list.append(line.strip())
+    pokemon_list_select = OptionMenu(background_label, selected_pokemon, *pokemon_list)
+    pokemon_list_select.place(relx = 0.125, rely = 0.25, anchor='center')
 
-# Container for the Stat labels.
-statLabels = Label(backgroundLabel, padx = 0, pady = 0, bg = "#133a5e")
-statLabels.place(relx = 0.71, rely = 0.578, anchor = "center")
-statValueLabels = []
-for i in range (0, 6):
-    statValueLabels.append(Label(statLabels, textvariable = statValues[i], font = ("Arial", 14), fg = "white", bg = "#133a5e"))
-    statValueLabels[i].place(anchor = "center")
-    if i == 0:
-        statValueLabels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
-    elif i == 5:
-        statValueLabels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
+    # Setup nature
+    nature_selected = StringVar(root)
+    if len(my_pokemon_list) == 0:
+        nature_selected.set("Bulbasaur")
     else:
-        statValueLabels[i].pack(side = "top", padx = 7, pady = 3.5)
+        nature_selected.set(my_pokemon_list[0][1])
+    nature_list = []
+    with open("Natures.txt", "r") as file:
+        for line in file:
+            nature_list.append(line.strip())
+    natures_list_select = OptionMenu(background_label, nature_selected, *nature_list)
+    natures_list_select.place(relx = 0.325, rely = 0.25, anchor = "center")
 
-# Initialize EV Variables
-evValues = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
-temp = 0
-for value in evValues:
-    if len(myPokemonList) == 0:
-        value.set(0)
+    # Setup level
+    pokemon_lvl = IntVar()
+    if len(my_pokemon_list) == 0:
+        pokemon_lvl.set(0)
     else:
-        value.set(myPokemonList[0][5][temp])
-    temp += 1
-    
-# Container for the EV labels.
-evLabels = Label(backgroundLabel, padx = 0, pady = 0, bg = "#133a5e")
-evLabels.place(relx = 0.83, rely = 0.578, anchor = "center")
-evValueLabels = []
-for i in range (0, 6):
-    evValueLabels.append(Label(evLabels, textvariable = evValues[i], font = ("Arial", 14), fg = "white", bg = "#133a5e"))
-    evValueLabels[i].place(anchor = "center")
-    if i == 0:
-        evValueLabels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
-    elif i == 5:
-        evValueLabels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
-    else:
-        evValueLabels[i].pack(side = "top", padx = 7, pady = 3.5)
+        pokemon_lvl.set(my_pokemon_list[0][2])
+    lvl_label = Label(background_label, padx = 7, pady = 0, textvariable = pokemon_lvl, font = ("Arial", 14), fg = "white", bg = "#1f5387")
+    lvl_label.place(relx = 0.6, rely = 0.185, anchor = "center")
 
+    # Initialize and Setup Stat Variables
+    stat_values = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
+    temp = 0
+    for value in stat_values:
+        if len(my_pokemon_list) == 0:
+            value.set(0)
+        else:
+            value.set(my_pokemon_list[0][4][temp])
+        temp += 1
+    stat_labels = Label(background_label, padx = 0, pady = 0, bg = "#133a5e")
+    stat_labels.place(relx = 0.71, rely = 0.578, anchor = "center")
+    stat_value_labels = []
+    for i in range (0, 6):
+        stat_value_labels.append(Label(stat_labels, textvariable = stat_values[i], font = ("Arial", 14), fg = "white", bg = "#133a5e"))
+        stat_value_labels[i].place(anchor = "center")
+        if i == 0:
+            stat_value_labels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
+        elif i == 5:
+            stat_value_labels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
+        else:
+            stat_value_labels[i].pack(side = "top", padx = 7, pady = 3.5)
 
-# Initialize IV Variables
-myIVs = calculateIVs(myPokemonList[0])
-ivValues = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
-temp = 0
-for value in ivValues:
-    if len(myPokemonList) == 0:
-        value.set(0)
-    else:
-        value.set(myIVs[temp])
-    temp += 1
+    # Initialize and Setup EV Variables
+    ev_values = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
+    temp = 0
+    for value in ev_values:
+        if len(my_pokemon_list) == 0:
+            value.set(0)
+        else:
+            value.set(my_pokemon_list[0][5][temp])
+        temp += 1
+    ev_labels = Label(background_label, padx = 0, pady = 0, bg = "#133a5e")
+    ev_labels.place(relx = 0.83, rely = 0.578, anchor = "center")
+    ev_value_labels = []
+    for i in range (0, 6):
+        ev_value_labels.append(Label(ev_labels, textvariable = ev_values[i], font = ("Arial", 14), fg = "white", bg = "#133a5e"))
+        ev_value_labels[i].place(anchor = "center")
+        if i == 0:
+            ev_value_labels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
+        elif i == 5:
+            ev_value_labels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
+        else:
+            ev_value_labels[i].pack(side = "top", padx = 7, pady = 3.5)
 
-# Container for the IV labels.
-ivLabels = Label(backgroundLabel, padx = 0, pady = 0, bg = "#133a5e")
-ivLabels.place(relx = 0.932, rely = 0.578, anchor = "center")
-ivValueLabels = []
-for i in range(0, 6):
-    ivValueLabels.append(Label(ivLabels, textvariable = ivValues[i], font = ("Arial", 14), fg = "white", bg = "#133a5e"))
-    ivValueLabels[i].place(anchor = "center")
-    if i == 0:
-        ivValueLabels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
-    elif i == 5:
-        ivValueLabels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
-    else:
-        ivValueLabels[i].pack(side = "top", padx = 7, pady = 3.5)
+    # Initialize and Setup IV Variables
+    my_ivs = calculate_ivs(my_pokemon_list[0])
+    iv_values = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
+    temp = 0
+    for value in iv_values:
+        if len(my_pokemon_list) == 0:
+            value.set(0)
+        else:
+            value.set(my_ivs[temp])
+        temp += 1
+    iv_labels = Label(background_label, padx = 0, pady = 0, bg = "#133a5e")
+    iv_labels.place(relx = 0.932, rely = 0.578, anchor = "center")
+    iv_value_labels = []
+    for i in range(0, 6):
+        iv_value_labels.append(Label(iv_labels, textvariable = iv_values[i], font = ("Arial", 14), fg = "white", bg = "#133a5e"))
+        iv_value_labels[i].place(anchor = "center")
+        if i == 0:
+            iv_value_labels[i].pack(side = "top", padx = 7, pady = (0, 3.5))
+        elif i == 5:
+            iv_value_labels[i].pack(side = "top", padx = 7, pady = (3.5, 0))
+        else:
+            iv_value_labels[i].pack(side = "top", padx = 7, pady = 3.5)
 
-root.mainloop()
+    root.mainloop()
+
+main()
