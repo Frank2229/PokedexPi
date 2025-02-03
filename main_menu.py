b70ff87ea4ed1ec1ed79ba5fb05d65381df3e7cb
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2025-02-03 11:12:20 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,11 +1,19 @@
-from tkinter import *
+import tkinter as tk
+import os
+
+def open_app(file_name):
+    '''Opens a selected application using a app name argument.'''
+    os.system(f"{file_name}")
+
 
 def main():
-    root = Tk()
+    root = tk.Tk()
     root.title("Pokedex Menu")
     root.geometry("480x320")
 
-    root.mainloop()
+    iv_calculator_button = tk.Button(root, text="Open IV Calculator", command=lambda: open_app("IVCalculator.py"))
+    iv_calculator_button.pack()
 
+    root.mainloop()
 
 main()
