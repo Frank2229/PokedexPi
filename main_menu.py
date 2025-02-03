5192344810416bf94d40663bcdf1aa0b70e1abdc
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2025-02-03 11:38:11 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -8,11 +8,18 @@ def open_app(file_name):
 
 def main():
     root = tk.Tk()
-    root.title("Pokedex Menu")
-    root.geometry("480x320")
+    root.title('Pokedex Menu')
+    root.geometry('480x320')
 
-    iv_calculator_button = tk.Button(root, text="Open IV Calculator", command=lambda: open_app("IVCalculator.py"))
-    iv_calculator_button.pack()
+    canvas = tk.Canvas(root, width=480, height=320)
+    canvas.pack()
+
+    bg_image = tk.PhotoImage(file='Images/menus/main_menu_background.png')
+    canvas.bg_image = bg_image
+    canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+    iv_calculator_button = tk.Button(root, text='IV Calculator', command=lambda: open_app('IVCalculator.py'))
+    canvas.create_window(360, 160, window=iv_calculator_button)
 
     root.mainloop()
 
