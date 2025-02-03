313ca3fb15a9aea4ce9d3037e2b9de6669154a4e
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2025-02-03 16:18:31 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 import os
 
-def open_app(file_name):
-    '''Opens a selected application using a app name argument.'''
-    os.system(f"{file_name}")
+def create_button(root, button_text, app_name, canvas, x_position, y_position):
+    '''Creates and returns a menu button.'''
+    button = tk.Button(root, text=button_text, command=lambda: open_app(app_name), bg='#256063', fg='white', height=2, width=15)
+    canvas.create_window(360+(1*x_position), 110+(50*y_position), window=button)
+
+    return button
 
 
-def main():
+def create_window():
+    '''Creates and returns the application window and background.'''
     root = tk.Tk()
     root.title('Pokedex Menu')
     root.geometry('480x320')
@@ -18,8 +22,20 @@ def main():
     canvas.bg_image = bg_image
     canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
-    iv_calculator_button = tk.Button(root, text='IV Calculator', command=lambda: open_app('IVCalculator.py'))
-    canvas.create_window(360, 160, window=iv_calculator_button)
+    return root, canvas
+
+
+def open_app(file_name):
+    '''Opens a selected application using a app name argument.'''
+    os.system(f"{file_name}")
+
+
+def main():
+    root, canvas = create_window()
+
+    pokemon_data_button= create_button(root, 'Pokemon Data', 'pokemon_data_viewer.py', canvas, 1, 0)  # Pokemon Data button
+    pokemon_data_button= create_button(root, 'Region Maps', 'region_map_viewer.py', canvas, 1, 1)   # Region Map button
+    pokemon_data_button= create_button(root, 'IV Calculator', 'iv_calculator.py', canvas, 1, 2) # IV Calculator button
 
     root.mainloop()
 
