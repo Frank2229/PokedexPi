29c323f9e4e2c5d294534add6030e23fcad3a41f
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2025-02-04 18:04:41 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,24 +1,26 @@
 import tkinter as tk
 import os
 
-def create_button(root, button_text, app_name, canvas, x_position, y_position):
-    '''Creates and returns a menu button.'''
-    button = tk.Button(root, text=button_text, command=lambda: open_app(app_name), bg='#256063', fg='white', height=2, width=15)
-    canvas.create_window(360+(1*x_position), 110+(50*y_position), window=button)
-
-    return button
+def change_menu_selection(event):
+    global current_selection
+    if event.keysym == "Up" and current_selection > 0:
+        canvas.move(arrow_id, 0, up_menu_offset)
+        current_selection -= 1
+    elif event.keysym == "Down" and current_selection < 5:
+        canvas.move(arrow_id, 0, down_menu_offset)
+        current_selection += 1
 
 
 def create_window():
     '''Creates and returns the application window and background.'''
     root = tk.Tk()
-    root.title('Pokedex Menu')
+    root.title('PokedexPI Menu')
     root.geometry('480x320')
 
     canvas = tk.Canvas(root, width=480, height=320)
     canvas.pack()
 
-    bg_image = tk.PhotoImage(file='Images/menus/main_menu_background.png')
+    bg_image = tk.PhotoImage(file='images/menus/main_menu_background.png')
     canvas.bg_image = bg_image
     canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
@@ -30,12 +32,42 @@ def open_app(file_name):
     os.system(f"{file_name}")
 
 
+def select_menu(event):
+    if current_selection == 0:
+        open_app('pokemon_data_viewer.py')
+    elif current_selection == 1:
+        open_app('region_map_viewer.py')
+    elif current_selection == 2:
+        open_app('iv_calculator.py')
+    elif current_selection == 3:
+        open_app('iv_calculator.py')
+    elif current_selection == 4:
+        open_app('iv_calculator.py')
+    elif current_selection == 5:
+        open_app('iv_calculator.py')
+
+
 def main():
+    global canvas
     root, canvas = create_window()
 
-    pokemon_data_button= create_button(root, 'Pokemon Data', 'pokemon_data_viewer.py', canvas, 1, 0)  # Pokemon Data button
-    pokemon_data_button= create_button(root, 'Region Maps', 'region_map_viewer.py', canvas, 1, 1)   # Region Map button
-    pokemon_data_button= create_button(root, 'IV Calculator', 'iv_calculator.py', canvas, 1, 2) # IV Calculator button
+    '''Arrow selector'''
+    arrow_select_image = tk.PhotoImage(file='images/cursors/arrow.png')
+    canvas.arrow_select_image = arrow_select_image
+    global arrow_id
+    arrow_id = canvas.create_image(295, 50, image=arrow_select_image)
+
+    '''Global variables for the movement of the selector arrow'''
+    global up_menu_offset, down_menu_offset
+    global current_selection
+    current_selection = 0
+    up_menu_offset = -44
+    down_menu_offset = 44
+
+    '''Keybindings'''
+    root.bind("<Up>", change_menu_selection)
+    root.bind("<Down>", change_menu_selection)
+    root.bind("<Return>", select_menu)
 
     root.mainloop()
 
